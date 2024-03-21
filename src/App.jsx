1a2986d64231ef842967d51dab977b9516a971ac
RobinShrestha <robinshrestha@lftechnology.com>
2024-03-21 12:24:25 +0545

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={reactLogo} />
+        HEllooowoowowowowowowow
+      </div>
+    </>
+  );
+}
+
+export default App;
